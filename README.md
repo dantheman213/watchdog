b6bc9bd13aad453645abb972e197479aad1efb90
Dan Gillespie <daniel.gillespie0949@gmail.com>
2020-05-22 20:39:01 -0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# watchdog
+
+Keep your server happy with auto-detection, monitoring, and reporting on your hard disks (S.M.A.R.T.), ZFS cluster(s), and UPS hardware.
+
+## Getting Started
+
+```
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y nut smartmontools
+
+echo "NOTE: if ZFS is not installed or configured then crontab should be modified after install"
+
+sudo cp -Rav rootfs/* /
+
+cat << EOF >> /var/spool/cron/crontabs/$(whoami)
+# Run SMART - short disk check every 12 hours
+0 */12 * * * bash /usr/bin/check-disks.sh short > /var/log/check-disks.log 2>&1
+
+# Run SMART - long disk once a week on Sunday
+0 0 * * 6 bash /usr/bin/check-disks.sh long > /var/log/check-disks.log 2>&1
+
+# Run a complete data check on the pool approx every 2 weeks
+0 0 */15 * * /usr/sbin/zpool scrub > /var/log/zpool/scrub.log
+EOF
+
+# TODO: check zfs pool status
+
+# TODO: send issues via email, slack, sms, etc
