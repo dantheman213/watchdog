4bf1f08c072bdb18145473cadcc68497d210a369
Dan Gillespie <daniel.gillespie0949@gmail.com>
2020-06-16 22:49:50 -0700

    testing

--- pkg/report/report.go
+++ pkg/report/report.go
@@ -7,7 +7,6 @@ import (
     "github.com/dantheman213/watchdog/pkg/common"
     "github.com/dantheman213/watchdog/pkg/config"
     "log"
-    "math"
     "strings"
     "time"
 )
@@ -36,19 +35,23 @@ func Start() {
 func startWeekly() {
     for true {
         log.Println("Report Weekly Scheduler timer has activated...")
-        now := time.Now()
-        target := common.CalculateEndDate(now, time.Saturday)
-        delta := now.Sub(target)
 
-        sleepSecs := math.Abs(delta.Seconds())
-        log.Printf("Sleeping until %s (%s or %f seconds)\n", target.String(), delta.String(), sleepSecs)
-        time.Sleep(time.Duration(sleepSecs) * time.Second)
+        // TODO: fix
+        //now := time.Now()
+        //target := common.CalculateEndDate(now, time.Saturday)
+        //delta := now.Sub(target)
+        //
+        //sleepSecs := math.Abs(delta.Seconds())
+        //log.Printf("Sleeping until %s (%s or %f seconds)\n", target.String(), delta.String(), sleepSecs)
+        //time.Sleep(time.Duration(sleepSecs) * time.Second)
 
-        getSMARTReports()
+        generateReports()
+        time.Sleep(time.Duration(999) * time.Second)
     }
 }
 
-func getSMARTReports() {
+func generateReports() {
+    log.Print("Generating Reports...")
     report := ""
 
     disks, err := common.GetDisks()
@@ -57,6 +60,8 @@ func getSMARTReports() {
     }
 
     for _, disk := range *disks {
+        log.Printf("Gathering info on disk %s for report...", disk)
+
         report += fmt.Sprintf("\n\nDisk Path: %s\n", disk)
         o, _, err := cli.RunCommand(fmt.Sprintf(`/usr/sbin/smartctl -i %s | grep -e SMART -e Available -e "Model Family" -e "Device Model" -e "Serial Number"`, disk))
         if err != nil {
@@ -89,5 +94,6 @@ func getSMARTReports() {
         }
     }
 
+    log.Println("preparing to send report email")
     sendEmail(config.Storage.EmailAccount.Address, "Watchdog Diagnostics Server Results", report)
 }
