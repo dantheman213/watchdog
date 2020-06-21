2b5418ef4838108451e9b1041c607b73bde7dc93
Dan Gillespie <daniel.gillespie0949@gmail.com>
2020-06-20 18:25:43 -0700

    improved email

--- pkg/report/email.go
+++ pkg/report/email.go
@@ -8,14 +8,18 @@ import (
 )
 
 func sendEmail(to, subject, body string) {
-    from := config.Storage.EmailAccount.Address
+    headerFrom := config.Storage.EmailAccount.Address
+    headerTo := fmt.Sprintf("To: %s\n", to)
+    headerSubject := fmt.Sprintf("Subject: %s\n", subject)
+    headerMIME := fmt.Sprintf(`MIME-version: 1.0;\nContent-Type: text/plain; charset="UTF-8";\n\n`)
+    msg := fmt.Sprintf("%s%s%s%s\n%s", headerFrom, headerTo, headerSubject, headerMIME, body)
+
     pass := config.Storage.EmailAccount.Password
-
-    msg := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n%s", from, to, subject, body)
-
-    err := smtp.SendMail(fmt.Sprintf("%s:%d", config.Storage.EmailAccount.SMTPHost, config.Storage.EmailAccount.SMTPPort),
-        smtp.PlainAuth("", from, pass, config.Storage.EmailAccount.SMTPHost),
-        from, []string{to}, []byte(msg))
+    err := smtp.SendMail(
+                fmt.Sprintf("%s:%d", config.Storage.EmailAccount.SMTPHost, config.Storage.EmailAccount.SMTPPort),
+                smtp.PlainAuth("", config.Storage.EmailAccount.Address, pass, config.Storage.EmailAccount.SMTPHost),
+                config.Storage.EmailAccount.Address, []string{to}, []byte(msg),
+            )
 
     if err != nil {
         log.Printf("smtp error: %s", err)
