5246dc6c257f98fa7936dde72e00291005a46d30
Dan Gillespie <daniel.gillespie0949@gmail.com>
2020-06-29 11:25:57 -0700

    minor

--- pkg/report/email.go
+++ pkg/report/email.go
@@ -14,10 +14,9 @@ func sendEmail(to, subject, body string) {
     headerMIME := fmt.Sprintf(`MIME-version: 1.0;\nContent-Type: text/plain; charset="UTF-8";\n\n`)
     msg := fmt.Sprintf("%s%s%s%s\n%s", headerFrom, headerTo, headerSubject, headerMIME, body)
 
-    pass := config.Storage.EmailAccount.Password
     err := smtp.SendMail(
                 fmt.Sprintf("%s:%d", config.Storage.EmailAccount.SMTPHost, config.Storage.EmailAccount.SMTPPort),
-                smtp.PlainAuth("", config.Storage.EmailAccount.Address, pass, config.Storage.EmailAccount.SMTPHost),
+                smtp.PlainAuth("", config.Storage.EmailAccount.Address, config.Storage.EmailAccount.Password, config.Storage.EmailAccount.SMTPHost),
                 config.Storage.EmailAccount.Address, []string{to}, []byte(msg),
             )
 
